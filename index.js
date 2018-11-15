e1f9c55bbfea4f76b57f78f4ea5c330ec00fad29
Truman <gongcunji@gmail.com>
2018-11-15 15:06:04 +0800

    Add header

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    book:{
+        assets: "./assets",
+        css: ["style.css"],
+        js: ["inter.js"]
+    },
+    // Map of hooks
+    hooks: {},
+
+    // Map of new blocks
+    blocks: {
+
+    },
+
+    // Map of new filters
+    filters: {}
+}
